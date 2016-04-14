d9dccea5e14e8d66d03522c42bf33bd5e6192602
zhangzhoufei <zhangzhoufei@2144.cn>
2016-04-14 17:28:56 +0800

    add yii2-redis、dynagrid、yii2-admin etc.

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,9 +1,10 @@
 <?php
+
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require(__DIR__.'/../../common/config/params.php'),
+    require(__DIR__.'/../../common/config/params-local.php'),
+    require(__DIR__.'/params.php'),
+    require(__DIR__.'/params-local.php')
 );
 
 return [
@@ -17,9 +18,11 @@ return [
             'enableAutoLogin' => true,
         ],*/
         'user' => [
+            'identityClass' => \mdm\admin\models\User::class,
+            'loginUrl' => ['admin/user/login'],
             'identityCookie' => [
-                'name'     => '_backendIdentity',
-                'path'     => '/admin',
+                'name' => '_backendIdentity',
+                'path' => '/admin',
                 'httpOnly' => true,
             ],
         ],
@@ -27,7 +30,7 @@ return [
             'name' => 'BACKENDSESSID',
             'cookieParams' => [
                 'httpOnly' => true,
-                'path'     => '/admin',
+                'path' => '/admin',
             ],
         ],
         'log' => [
@@ -51,19 +54,39 @@ return [
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:[a-zA-Z0-9-_]+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-            ]
+            ],
         ],
     ],
     'modules' => [
-        'user' => [
+        /*'user' => [
             // following line will restrict access to profile, recovery, registration and settings controllers from backend
             'as backend' => 'dektrium\user\filters\BackendFilter',
-            'controllers' => []
-        ],
+            'controllers' => [],
+        ],*/
         'rest-client' => [
             'class' => 'zhuravljov\yii\rest\Module',
             //'baseUrl' => 'http://localhost/api/v1',
             'allowedIPs' => ['127.0.0.1', '::1', '222.73.203.30'],
+        ],
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            'controllerMap' => [
+                'assignment' => [
+                    'class' => 'mdm\admin\controllers\AssignmentController',
+                ],
+            ],
+        ],
+    ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
         ],
     ],
     'params' => $params,
--- common/config/main.php
+++ common/config/main.php
@@ -5,13 +5,20 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ]
     ],
     'modules' => [
-        'user' => [
+        /*'user' => [
             'class' => 'dektrium\user\Module',
             // you will configure your module inside this file
             // or if need different configuration for frontend and backend you may
             // configure in needed configs
-        ],
+        ],*/
+    ],
+    'aliases' => [
+        '@mdm/admin' => '@vendor/mdmsoft/yii2-admin',
+        // for example: '@mdm/admin' => '@app/extensions/mdm/yii2-admin-2.0.0',
     ],
 ];
